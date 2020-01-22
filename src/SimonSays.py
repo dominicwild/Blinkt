f1043cf7b0a7226888cc0588931af57850865bdc
dwild8 <dwild8@dxc.com>
2020-01-22 16:11:57 +0000

    Moved code from example.py to SimonSays.py

--- src/SimonSays.py
+++ src/SimonSays.py
@@ -5,14 +5,18 @@ import os
 import csv
 from Button import Button
 from Color import Color
+import pygame
+import sys
 
 
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+
 def set_pixel(pixel, color):
     value = color.value
     set(pixel, value[0], value[1], value[2])
+
 
 class SimonSays():
     buttons = []
@@ -22,6 +26,8 @@ class SimonSays():
     speed = -1
     name = ""
     scoreFile = "scores.csv"
+    pressedButton = None
+    playing = False
 
     def __init__(self):
         self.addButton(Color.RED, [0, 1])
@@ -54,7 +60,7 @@ class SimonSays():
     def getNumberOfButtons(self):
         return len(self.buttons)
 
-    def getButtonColour(self,index):
+    def getButtonColour(self, index):
         return self.buttons[index].color
 
     def deleteButtons(self):
@@ -66,13 +72,13 @@ class SimonSays():
             colors.append(button.color)
         return colors
 
-    def getSpeed(self,options):
+    def getSpeed(self, options):
         while self.speed < 0:
             self.renderMenu(options)
             print("In this mode you select a speed and the LEDs will stay lit for that duration.")
             try:
                 self.speed = int(input("Select a speed\n> "))
-                if(self.speed < 0 and self.speed > 5):
+                if (self.speed < 0 and self.speed > 5):
                     print("That is not a valid speed, try again. It must be one of the options above.")
             except Exception:
                 print("The speed must be a number from the above.")
@@ -82,14 +88,15 @@ class SimonSays():
         while self.difficulty < 0:
             print("In this mode you can create your own custom difficulty.")
             try:
-                self.difficulty = int(input("Input a number to create your difficulty. The higher the number, the more lights you will have to remember in each round of the game, however the more points you'll get!\n> "))
-                if(self.difficulty < 0):
+                self.difficulty = int(input(
+                    "Input a number to create your difficulty. The higher the number, the more lights you will have to remember in each round of the game, however the more points you'll get!\n> "))
+                if (self.difficulty < 0):
                     print("That is not a valid difficulty, try again. It must be greater than zero.")
             except Exception:
                 print("The difficulty must be a number.")
                 self.difficulty = -1
 
-    #Since modes are only simplistic (only modifying points generated per round) no need to separate into classes. Although could be done in future if logic gets more complicated.
+    # Since modes are only simplistic (only modifying points generated per round) no need to separate into classes. Although could be done in future if logic gets more complicated.
     def classicMode(self):
         rand = randint(0, self.getNumberOfButtons() - 1)
         self.sequence.append(self.getButtonColour(rand))
@@ -97,29 +104,29 @@ class SimonSays():
 
         self.displaySequence(self.numSeq)
         return 1
-    
+
     def difficultyMode(self):
 
         self.getDifficulty()
-            
+
         for i in range(self.difficulty):
             rand = randint(0, self.getNumberOfButtons() - 1)
             self.sequence.append(self.getButtonColour(rand))
             self.numSeq.append(rand)
 
         self.displaySequence(self.numSeq)
-        return self.difficulty*self.difficulty
+        return self.difficulty * self.difficulty
 
     def speedRunMode(self):
 
         options = ["Slowest", "Slow", "Normal", "Fast", "Fastest"]
-        speeds = [2,1.5,1,0.5,0.25]
-        modifiers = [0.25,0.5,1,2,4]
+        speeds = [2, 1.5, 1, 0.5, 0.25]
+        modifiers = [0.25, 0.5, 1, 2, 4]
 
         self.getSpeed(options)
 
-        modifier =modifiers[self.speed-1]
-        duration = speeds[self.speed-1] #Assign actual speed value
+        modifier = modifiers[self.speed - 1]
+        duration = speeds[self.speed - 1]  # Assign actual speed value
 
         rand = randint(0, self.getNumberOfButtons() - 1)
         self.sequence.append(self.getButtonColour(rand))
@@ -131,13 +138,14 @@ class SimonSays():
 
     def multiMode(self):
 
-        if(self.getNumberOfButtons() == 4):
+        if (self.getNumberOfButtons() == 4):
             print("In this mode instead of 4 buttons, there are 8 you must memorise.")
             self.deleteButtons()
-            buttons = [[Color.RED, [0]],[Color.GREEN, [1]],[Color.YELLOW, [2]],[Color.BLUE, [3]],[Color.WHITE, [4]],[Color.ORANGE, [5]],[Color.MAROON, [6]],[Color.PURPLE, [7]]]
+            buttons = [[Color.RED, [0]], [Color.GREEN, [1]], [Color.YELLOW, [2]], [Color.BLUE, [3]], [Color.WHITE, [4]],
+                       [Color.ORANGE, [5]], [Color.MAROON, [6]], [Color.PURPLE, [7]]]
             for button in buttons:
                 self.addButton(button[0], button[1])
-        
+
         colorString = ""
         for color in self.getButtonColours():
             colorString += color.name + " "
@@ -147,43 +155,17 @@ class SimonSays():
         self.numSeq.append(rand)
 
         self.displaySequence(self.numSeq)
-        print("The available colours are: " + colorString) #Print colours in case user forgets or doesn't know what a particular colour may be
-        return 1 + 0.5*len(self.numSeq)
+        print(
+            "The available colours are: " + colorString)  # Print colours in case user forgets or doesn't know what a particular colour may be
+        return 1 + 0.5 * len(self.numSeq)
 
-    def textMode(self):
-        mode = "Invalid option"
-        while mode == "Invalid option":
-            self.renderMenu(["Classic Mode", "Difficulty Mode", "Speed Run Mode", "Multi Mode"])
-            option = input("Which mode do you want to play? (input the number):\n> ")
-            modes = {"1" : self.classicMode, "2": self.difficultyMode, "3": self.speedRunMode, "4" : self.multiMode}
-            mode = modes.get(option, "Invalid option")
-        print("Playing the text version of Simon.")
-        print("Write your guesses with spaces separating the colours i.e., 'y b r g' for guessing yellow, blue, red and then green, in that order")
-        playing = True
-        points = 0
-        numSeq = []
-        while playing:
-            modifier = mode()
-            self.reset()
-            correct = self.parseGuess(input("What was the sequence of lights?\n> ").strip(" ")) #Trim so that extra spaces don't invalidate user input
-            if (correct):
-                points += 1*modifier
-                cls()
-                print("That was correct. You have " + str(points) + " points")
-            else:
-                print("That was incorrect. You finished with " +
-                      str(points) + " points")
-                print("The correct sequence was " + self.getStringSequence())
-                self.storeScore(points)
-                self.resetGameState()
-                playing = False
-    
     def resetGameState(self):
         self.sequence = []
         self.numSeq = []
         self.difficulty = -1
         self.speed = -1
-        if(not self.getNumberOfButtons() == 4):
+        self.playing = False
+        if (not self.getNumberOfButtons() == 4):
             self.deleteButtons()
             self.addButton(Color.RED, [0, 1])
             self.addButton(Color.GREEN, [2, 3])
@@ -192,12 +174,12 @@ class SimonSays():
 
     def parseGuess(self, guess):
         guessedSequence = guess.split(" ")
-        
-        if(not len(self.sequence) == len(guessedSequence)):
+
+        if (not len(self.sequence) == len(guessedSequence)):
             return False
 
         for i in range(len(self.sequence)):
-            if(Color.toColor(guessedSequence[i]) == self.sequence[i]):
+            if (Color.toColor(guessedSequence[i]) == self.sequence[i]):
                 continue
             else:
                 return False
@@ -217,9 +199,9 @@ class SimonSays():
 
     def start(self):
         while True:
-            self.renderMenu(["Text Based", "View Leaderboard"])
+            self.renderMenu(["Text Based", "GUI Based", "View Leaderboard"])
             option = input("Select an option: ")
-            mode = {"1": self.textMode, "2": self.printLeaderBoard}
+            mode = {"1": self.textMode, "2": self.guiMode, "3": self.printLeaderBoard}
             toPlay = mode.get(option, False)
             if (toPlay):
                 toPlay()
@@ -229,23 +211,22 @@ class SimonSays():
     def renderMenu(self, menu):
         length = 40
         i = 0
-        print("#"*length)
+        print("#" * length)
         for option in menu:
             i += 1
-            print("# " + str(i) + ". " + option +" "*(length - 5 - len(str(i)) - len(option)) + "#")
-        print("#"*length)
-
+            print("# " + str(i) + ". " + option + " " * (length - 5 - len(str(i)) - len(option)) + "#")
+        print("#" * length)
 
     def getName(self):
         while len(self.name) == 0 or len(self.name) > 12:
-            if(len(self.name) == 0):
+            if (len(self.name) == 0):
                 print("You must enter a name.")
             if (len(self.name) > 12):
                 print("That name is too long.")
             self.name = input("What is your name? (for the leaderboard)\n> ")
 
     def storeScore(self, points):
-        if(self.name == ""):
+        if (self.name == ""):
             # Only ask for name when necessary
             self.getName()
         try:
@@ -254,37 +235,139 @@ class SimonSays():
                 reader = sorted(csv.reader(scores), key=lambda row: row[1], reverse=True)
                 saved = False
                 for row in reader:
-                    if(row[0] == self.name):
-                        if(round(float(row[1])) < points):
+                    if (row[0] == self.name):
+                        if (round(float(row[1])) < points):
                             row[1] = points
                             print("Your score has been updated in the leaderboard.")
                         else:
                             print("You already have a higher score in the leaderboard.")
                         saved = True
-                    
-                if(saved == False):
-                    reader.append([self.name,points])
-                reader.sort(key=lambda row: int(row[1]),reverse=True) #Sort at saving, rather than leaderboard as this could be put on a background thread if it becomes cumbersome and users would get more frustrating if the leaderboard took a long time to load, than they would saving.
+
+                if (saved == False):
+                    reader.append([self.name, points])
+                reader.sort(key=lambda row: int(row[1]),
+                            reverse=True)  # Sort at saving, rather than leaderboard as this could be put on a background thread if it becomes cumbersome and users would get more frustrating if the leaderboard took a long time to load, than they would saving.
                 writer.writerows(reader)
 
         except Exception as e:
             print(e)
 
-
         os.replace("temp.csv", self.scoreFile)
-
 
     def printLeaderBoard(self):
         leaders = []
         with open(self.scoreFile, "r") as scores:
             reader = csv.reader(scores)
-            i= 0
+            i = 0
             negativePadding = 0
             for row in reader:
                 i += 1
-                if(len(str(i)) ==2 ):
+                if (len(str(i)) == 2):
                     negativePadding = -1
-                leaders.append(row[0] + " "*(12 - len(row[0]) + negativePadding) + row[1])
-                if(i == 10):
+                leaders.append(row[0] + " " * (12 - len(row[0]) + negativePadding) + row[1])
+                if (i == 10):
                     break
         self.renderMenu(leaders)
+
+    def textMode(self):
+        mode = "Invalid option"
+        while mode == "Invalid option":
+            self.renderMenu(["Classic Mode", "Difficulty Mode", "Speed Run Mode", "Multi Mode"])
+            option = input("Which mode do you want to play? (input the number):\n> ")
+            modes = {"1": self.classicMode, "2": self.difficultyMode, "3": self.speedRunMode, "4": self.multiMode}
+            mode = modes.get(option, "Invalid option")
+        print("Playing the text version of Simon.")
+        print(
+            "Write your guesses with spaces separating the colours i.e., 'y b r g' for guessing yellow, blue, red and then green, in that order")
+        self.playing = True
+        points = 0
+        numSeq = []
+        while self.playing:
+            modifier = mode()
+            self.reset()
+            correct = self.parseGuess(input("What was the sequence of lights?\n> ").strip(
+                " "))  # Trim so that extra spaces don't invalidate user input
+            if (correct):
+                points += 1 * modifier
+                cls()
+                print("That was correct. You have " + str(points) + " points")
+            else:
+                print("That was incorrect. You finished with " +
+                      str(points) + " points")
+                print("The correct sequence was " + self.getStringSequence())
+                self.storeScore(points)
+                self.resetGameState()
+                self.playing = False
+
+    def guiMode(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.fps = 60
+        size = width, height = [1000, 900]
+        midpoint = (width / 2, height / 2)
+        self.bg = [255, 255, 255]
+        guess = []
+        self.playing = True
+
+        self.screen = pygame.display.set_mode(size)
+
+        buttonHeight = height / 2
+        buttonWidth = width / 2
+
+        rect1 = pygame.Rect(midpoint[0], midpoint[1], buttonWidth, buttonHeight)
+        rect2 = pygame.Rect(midpoint[0] - buttonWidth, midpoint[1], buttonWidth, buttonHeight)
+        rect3 = pygame.Rect(midpoint[0] - buttonWidth, midpoint[1] - buttonHeight, buttonWidth, buttonHeight)
+        rect4 = pygame.Rect(midpoint[0], midpoint[1] - buttonHeight, buttonWidth, buttonHeight)
+
+        rects = [rect1, rect2, rect3, rect4]
+
+        for i in range(len(rects)):
+            self.buttons[i].setDraw(self.screen, rects[i])
+
+        while self.playing:
+
+            self.handleEvents()
+            self.draw()
+
+        self.resetGameState()
+
+    def handleEvents(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                self.playing = False
+                break
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos  # gets mouse position
+
+                # checks if mouse position is over the button
+                for button in self.buttons:
+                    if button.rect.collidepoint(mouse_pos):
+                        # prints current location of mouse
+                        print("Button pressed: " + button.color.name)
+                        button.pressed()
+                        self.pressedButton = button
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse_pos = event.pos  # gets mouse position
+
+                # checks if mouse position is over the button
+                for button in self.buttons:
+                    button.resetGUIColor()
+                    if button.rect.collidepoint(mouse_pos):
+                        # prints current location of mouse
+                        print("Button released: " + button.color.name)
+                        if (self.pressedButton and self.pressedButton == button):
+                            button.addColor(self.sequence)
+                            print(self.sequence)
+                self.pressedButton = None
+
+    def draw(self):
+        if (self.playing):
+            self.screen.fill(self.bg)
+            for button in self.buttons:
+                pygame.draw.rect(self.screen, button.getGUIColor(), button.rect)  # draw button
+
+            pygame.display.update()
+            self.clock.tick(self.fps)
